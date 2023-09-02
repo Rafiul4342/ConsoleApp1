622bad72f0969cb2195a909a1055ccc3dfa0427a
Rafiul Islam <rafiul.islam@ovgu.de>
2023-09-02 11:11:59 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,217 @@
+﻿
+using BaSyx.AAS.Client.Http;
+using BaSyx.Models.Core.AssetAdministrationShell.Generics;
+using BaSyx.Models.Core.AssetAdministrationShell.Implementations;
+using BaSyx.Models.Core.Common;
+using BaSyx.Models.Extensions;
+using MqttClientSouthboundSimulatedvalues;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string path = "Maintenance_1/MaintenanceRecord/";
+        string aaspath = "http://localhost:5180";
+
+
+        string json = @"
+       {
+            ""idShort"": ""MaintenanceRecord"",
+            ""modelType"": {
+                ""name"": ""SubmodelElementCollection""
+            },
+            ""value"": [
+                {
+                    ""idShort"": ""MaintenanceStart"",
+                    ""modelType"": {
+                        ""name"": ""Property""
+                    },
+                    ""value"": ""12"",
+                    ""valueType"": ""string""
+                },
+                {
+                    ""idShort"": ""MaintenanceEnd"",
+                    ""modelType"": {
+                        ""name"": ""Property""
+                    },
+                    ""value"": ""12jnkjn"",
+                    ""valueType"": ""string""
+                },
+                {
+                    ""idShort"": ""MaintenanceCompletionTime"",
+                    ""modelType"": {
+                        ""name"": ""Property""
+                    },
+                    ""value"": 0.0,
+                    ""valueType"": ""double""
+                },
+                {
+                    ""idShort"": ""MaintenanceStaff"",
+                    ""modelType"": {
+                        ""name"": ""Property""
+                    },
+                    ""value"": ""100000"",
+                    ""valueType"": ""string""
+                },
+                {
+                    ""idShort"": ""MaintenanceCost"",
+                    ""modelType"": {
+                        ""name"": ""Property""
+                    },
+                    ""value"": 1000,
+                    ""valueType"": ""double""
+                }
+]
+        }";
+        /*
+            var Client = new HttpClient();
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var apiUrl = string.Concat(aaspath,"/", path);
+            Console.WriteLine(apiUrl);
+
+           var response = await Client.PutAsync("http://localhost:5180/Maintenance_1/MaintenanceRecord/Record1", content);
+
+           if (response.IsSuccessStatusCode)
+           {
+               // The update was successful
+               Console.WriteLine("Submodel element collection updated successfully.");
+           }
+           else
+           {
+               // Handle errors or error responses from the server
+               Console.WriteLine("Failed to update submodel element collection.");
+           }
+
+            SubmodelElementCollection submodelElementColl = JsonConvert.DeserializeObject<SubmodelElementCollection>(json);
+            //Console.WriteLine(submodelElementColl.GetType());
+            Console.WriteLine(submodelElementColl.ToJson());
+            string result = JsonConvert.SerializeObject(submodelElementColl) as string;
+            Console.WriteLine(result);
+            submodelElementColl.IdShort = "Record2";
+
+        */
+           
+            var aasClient = new AssetAdministrationShellHttpClient(new Uri(aaspath));
+
+
+        /*    
+                foreach (var submodel in submodelElementColl.Value)
+                    {
+
+                    var vale = submodel.GetValue();
+                    Console.WriteLine(submodel.IdShort);
+                    Console.WriteLine(vale.Value);
+                    try
+                    {
+                      var updv=  aasClient.UpdateSubmodelElementValue("MaintenanceSubmodel", string.Concat(path,"/",submodel.IdShort), vale);
+                        if (updv.Success) { Console.WriteLine("value updated"); }
+                        else { Console.WriteLine("Vale not updated"); }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+
+                    }
+
+                */
+        SubmodelElementCollection submodelElementColl = JsonConvert.DeserializeObject<SubmodelElementCollection>(json);
+        //Console.WriteLine(submodelElementColl.GetType());
+        Console.WriteLine(submodelElementColl.ToJson());
+        string result = JsonConvert.SerializeObject(submodelElementColl) as string;
+        Console.WriteLine(result);
+        try
+        {
+
+            try
+            {
+                 var submodelElementCol = JsonConvert.DeserializeObject<SubmodelElementCollection>(json);
+
+                Console.WriteLine(submodelElementColl);
+                var data = submodelElementColl.Value[0];
+                Console.WriteLine(data.GetType());
+                Console.WriteLine($"This is the data we are trying to update {0}",data);
+
+                var update = aasClient.CreateOrUpdateSubmodelElement("MaintenanceSubmodel", path , data);
+                Console.WriteLine("Value updated");
+                Console.WriteLine(update.Messages);
+
+                if (update.Success)
+                {
+                    Console.WriteLine("updata successfull");
+                }
+                else
+                {
+                    Console.WriteLine("Not successful");
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+           
+            var datanew = aasClient.RetrieveSubmodelElement("MaintenanceSubmodel", path);
+            Console.WriteLine(datanew.Entity.ToJson());
+        } 
+      
+        catch(Exception ex) 
+        {
+        Console.WriteLine(ex.Message);
+        }
+        
+       
+       
+        /*
+       
+
+        try
+        {
+            // Specify the path to the MaintenanceRecord in the AAS
+            string submodelPath = "Maintenance_1/MaintenanceRecord";
+
+            // Retrieve the existing MaintenanceRecord from the AAS
+            var existingRecord =  aasClient.RetrieveSubmodelElement("MaintenanceSubmodel", submodelPath);
+
+            if (existingRecord != null)
+            {
+                Console.WriteLine(existingRecord.Entity.ToJson());
+                // Update the existing record with the new values
+                existingRecord.Entity.SetValue(submodelElementColl);
+                
+                
+              Console.WriteLine(existingRecord.Entity.ToJson());
+               var data1 = existingRecord.Entity.ToJson();
+
+                var collection = JsonConvert.DeserializeObject<SubmodelElementCollection>(data1);
+          
+
+                var updateResult = aasClient.CreateOrUpdateSubmodelElement("MaintenanceSubmodel", submodelPath,collection); 
+          
+                if (updateResult.Success)
+                {
+                    Console.WriteLine("MaintenanceRecord updated successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to update MaintenanceRecord: " );
+                }
+            }
+            else
+            {
+                Console.WriteLine("MaintenanceRecord not found in the AAS.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+        // Display the number of command line arguments.
+       */
+       // UpdateSubmodelElement e = new UpdateSubmodelElement(aaspath);
+     //  var data = await e.getData(path);
+      // await e.UpdateSubmodelElementVale(path, submodelElementColl);
+       // await e.UpdateSubmodelElementVale(path, data);*/
+    }
+}
---
+++ ConsoleApp1/UpdateSubmodelElement.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
+using BaSyx.AAS.Client.Http;
+using BaSyx.Models;
+using BaSyx.Models.Core.AssetAdministrationShell.Generics;
+using BaSyx.Models.Core.AssetAdministrationShell.Implementations;
+using BaSyx.Models.Core.Common;
+using BaSyx.Models.Extensions;
+using BaSyx.Utils.ResultHandling;
+using Newtonsoft.Json;
+
+namespace MqttClientSouthboundSimulatedvalues
+{
+
+    public class UpdateSubmodelElement
+    {
+        public AssetAdministrationShellHttpClient client;
+
+        public static SubmodelElementCollection? coll;
+        public SubmodelElementCollection newCollection { get; set; }
+        static int i = 0;
+
+      public UpdateSubmodelElement(string url)
+        {
+            client = new AssetAdministrationShellHttpClient(new Uri(url));
+            
+        }
+
+
+        public async Task UpdateRecords(string path, SubmodelElementCollection Collection) { 
+        await Task.Delay(1000);
+        }
+        public async Task UpdateSubmodelElementVale(String Path,SubmodelElementCollection Col)
+        {
+            await Task.Delay(1000);
+            var data =JsonConvert.SerializeObject(Col);
+            Console.WriteLine(data);
+
+            try
+            {
+                var record = client.RetrieveSubmodelElement("MaintenanceSubmodel", Path);
+                if (record.Success && record!= null) 
+                {
+                    var type = Col.GetType();
+                    var v = Col.Value;
+                    Console.WriteLine(v.GetType());
+
+                    record.Entity.SetValue(Col);
+                    ISubmodelElement submodelElement = record.Entity;
+                 //   ISubmodelElement submodelElement1 = JsonConvert.DeserializeObject<ISubmodelElement>(record.Entity.ToJson());
+                // List<ISubmodelElement> Element= new List<ISubmodelElement>();
+                // Element.Add(submodelElement);
+
+
+
+
+
+                    try
+                    {
+                        var update = client.CreateOrUpdateSubmodelElement("MaintenanceSubmodel", "Maintenance_1", submodelElement);
+                        Console.WriteLine(update.Messages);
+                        if (update.Success)
+                        {
+                            Console.WriteLine("Value Update");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not Updated");
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                   
+                    var vale = record.GetType();
+                    var val = record.Entity.ToJson();
+
+
+                    Console.WriteLine(val);
+                    Console.WriteLine(vale.ToString());
+                    
+                 
+                    
+                    
+               //  var updatedrecord = JsonConvert.DeserializeObject<SubmodelElementCollection>(updatejson);
+                  //var value = updatedrecord.GetValue().GetType(); 
+                    //Console.WriteLine($"Updated {value}");
+                  //  Console.WriteLine(v.Value.ToJson());
+                    try {
+                        var UpdatedRecord = client.CreateOrUpdateSubmodelElement("MaintenanceSubmodel", string.Concat(Path,"_",i.ToString()), (ISubmodelElement)v.Values);
+                        i++;
+                        if (UpdatedRecord.Success)
+                        {
+                            Console.WriteLine(UpdatedRecord);
+                        }
+                        else
+                        {
+                            Console.WriteLine("RecordNotupdated");
+                        }
+                    } 
+                    catch(Exception ex)
+                    { 
+                        Console.WriteLine(ex.ToString());
+                    }
+                    
+                    
+
+                }
+
+            }
+            catch 
+            (Exception ex) 
+            
+            { Console.WriteLine(ex.Message); }
+            
+          
+            
+          
+         //   var submodelElment = client.RetrieveSubmodelElement("MaintenanceSubmodel",Path);
+           // Console.WriteLine(submodelElment.Entity.ToJson());
+
+           
+              
+                 // submodelElment.Entity.SetValue(newCollection);
+                    Console.WriteLine(client.RetrieveSubmodelElement("MaintenanceSubmodel",Path).Entity.ToJson());
+                try
+                {
+                    var update = client.CreateOrUpdateSubmodelElement("MaintenanceSubmodel", Path, newCollection);
+                    Console.WriteLine(client.RetrieveSubmodelElement("MaintenanceSubmodel", Path).Entity.ToJson());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                   
+                    
+               
+              
+       }
+
+     
+
+        public async Task<SubmodelElementCollection?> getData(string path)
+        {
+            await Task.Delay(1000);
+
+            var data = client.RetrieveSubmodelElement("MaintenanceSubmodel", path);
+            if (data != null)
+            {
+                var vale = data.Entity.ToJson();
+                Console.WriteLine(vale);
+              
+
+           SubmodelElementCollection submodelElementColl = JsonConvert.DeserializeObject<SubmodelElementCollection>(vale);
+      
+
+                Console.WriteLine(submodelElementColl.GetType());
+                
+                    foreach(var item in submodelElementColl.Value)
+                {
+                    Console.WriteLine(item.IdShort);
+                    Console.WriteLine(item.GetValue().Value);
+                    if(item.IdShort == "MaintenanceStart")
+                    {
+                        IValue updatedValue = new ElementValue("10/27/2023 10:07:42 AM", typeof(string));
+                        item.SetValue(updatedValue);
+                        Console.WriteLine(item.GetValue().Value);
+                        submodelElementColl.SetValue(item);
+                      
+                    }
+                }
+
+                data.Entity.SetValue(submodelElementColl.Value);
+                Console.WriteLine(data.Entity.ToJson());
+
+             //  var update= client.CreateOrUpdateSubmodelElement(path, data);
+               
+                Console.WriteLine(JsonConvert.SerializeObject(submodelElementColl));   
+                return submodelElementColl;
+
+            }
+            else
+            {
+                return null;
+            }
+
+           
+        }
+
+    }
+}
